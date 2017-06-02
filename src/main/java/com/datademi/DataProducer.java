0eb31cd34a10b64c7ce4cecf3a358e910e5e163c
Safak Serdar Kapci <sskapci@gmail.com>
2017-06-02 20:40:49 +0300

    DataProducer
    Signed-off-by: sskapci <sskapci@gmail.com>

---
+++ src/main/java/com/datademi/DataProducer.java
@@ -0,0 +1,28 @@
+package com.datademi;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * Created by genel on 6/2/17.
+ */
+public class DataProducer {
+    public static void main(String[] args) throws Exception{
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for(int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+
+        producer.close();
+    }
+}
